e0c2f57471653f6b71d3d38d74e27636a015a14f
anhvt25 <theanhvu1501@gmail.com>
2025-05-10 22:49:41 +0700

    update audio download video

--- download.js
+++ download.js
@@ -14,7 +14,7 @@ const downloadVideo = async (url, outputPath) => {
     const output = path.join(outputPath, "%(title)s.%(ext)s");
     await youtubedl(url, {
       output: output,
-      format: "bestvideo[height=720]+bestaudio[ext=m4a]/mp4",
+      format: "bestvideo[height=720]+bestaudio/best",
       mergeOutputFormat: "mp4",
       writeThumbnail: true,
       convertThumbnails: "jpg",
